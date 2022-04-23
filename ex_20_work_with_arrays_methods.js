5a6613232faa93c7bd62e1ed70510e0ab650bd64
Timofey <timofeyvalerievich@gmail.com>
2022-04-23 17:05:50 +0300

    ex_20_from_course

---
+++ ex_20_work_with_arrays_methods.js
@@ -0,0 +1,53 @@
+const films = [
+    { name: 'Titanic', rating: 9, id: 0 },
+    { name: 'Die hard 5', rating: 5, id: 1 },
+    { name: 'Matrix', rating: 8, id: 2 },
+    { name: 'Some bad film', rating: 4, id: 3 }
+];
+
+// проверка массива на рейтинг фильмов, который больше или равен 8;
+
+function showGoodFilms(arr) {
+   let newArr = [];
+   arr.map(film => {
+       if(film.rating >= 8) {
+           newArr.push(film);
+       }
+   });
+   return newArr;
+}
+
+console.log(showGoodFilms(films));
+
+//возврат строки с названиями фильмов из массива;
+
+function showListOfFilms(arr) {
+   let nameFilm = arr.map(film => film.name);
+   return nameFilm.join(', ');
+}
+
+console.log(showListOfFilms(films));
+
+//добавление елемента id к каждобу объекту массива;
+
+function setFilmsIds(arr) {
+    return arr.map((film, i) => {
+        film.id = i;
+        return film;
+    });
+}
+
+const tranformedArray = setFilmsIds(films);
+
+console.log(tranformedArray);
+
+//проверка массива на то, что в каждом объекте есть id;
+
+function checkFilms(arr) {
+    return arr.every(film => film.id || film.id === 0);
+}
+
+console.log(checkFilms(tranformedArray));
+
+
+
