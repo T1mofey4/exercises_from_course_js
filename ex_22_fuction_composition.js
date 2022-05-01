a7f56432b0a039357e5be5d7dea980fb9a6a167f
Timofey <timofeyvalerievich@gmail.com>
2022-05-01 20:18:36 +0300

    added ex: 22

---
+++ ex_22_fuction_composition.js
@@ -0,0 +1,19 @@
+
+const multiply20 = (price) => price * 20,
+      divide100 = (price) => price / 100,
+      normalizePrice = (price) => price.toFixed(2);
+
+const compose = (...fns) => (x) => fns.reduceRight((res, fn) => fn(res), x);
+
+console.log(compose(multiply20, divide100, normalizePrice)(200));
+
+
+
+
+
+const add1 = function(a) {return a + 1},
+      addA113 = function(a, b, c){return a + b + c};
+
+const composeWithArgs = (...fns) => fns.reduceRight((f, g) => (...args) => g(f(...args)));
+
+console.log(composeWithArgs(add1, addA113)(1, 2, 3));
